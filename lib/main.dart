2e766b90f4d140a14e1f3b16bd524c481870ad33
NJONTU MADEH Venus Grace <venusgrace44@gmail.com>
2026-09-17 01:16:47 +0100

    Initialisation du projet Docs Admins

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'views/splash_view.dart';
+
+void main() {
+  runApp(const DocsAdminsApp());
+}
+
+class DocsAdminsApp extends StatelessWidget {
+  const DocsAdminsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Docs Admins',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
---
+++ lib/views/splash_view.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOutBack,
+    );
+
+    _animationController.forward();
+
+    Timer(const Duration(seconds: 2), () {
+      if (!mounted) return;
+
+      
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ScaleTransition(
+          scale: _animation,
+          child: const Text(
+            'DOCS ADMINS',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
